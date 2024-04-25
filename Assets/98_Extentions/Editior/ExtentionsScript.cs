927d564714d746f0e90672d8f87af6ccd449421e
TAKETO-ANNAKA <antake1226@outlook.jp>
2024-04-25 15:49:02 +0900

    フォルダ整理

---
+++ Assets/98_Extentions/Editior/ExtentionsScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtentionsScript : MonoBehaviour
+{
+    /*エディターの拡張機能を追加する場合は /Editior 階層にスクリプトを生成し、そこへ記述する*/
+}
